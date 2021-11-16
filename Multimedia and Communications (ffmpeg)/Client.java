716c2d57d00863aa4d6a28e2c0f9d69522854238
dabusted <reportbot@hotmail.com>
2021-11-16 15:19:47 +0200

    Update Client.java

--- Multimedia and Communications (ffmpeg)/Client.java
+++ Multimedia and Communications (ffmpeg)/Client.java
@@ -44,7 +44,6 @@ public class Client {
 		
 		
 		
-//σχετικά με gui
 		JFrame frame = new JFrame();
         frame.setSize(400,400);//400 width and 500 height  
 		frame.setVisible(true);//making the frame visible  
@@ -60,14 +59,12 @@ public class Client {
         JScrollPane sp = new JScrollPane(tarea);
         sp.setBounds(100, 100, 100, 100);
         
-//jtextfield αποτυχημένο μεν γιατί υπήρχε bug όταν έπαιρνα το text        
+      
         JTextField tfield = new JTextField("write your text here ");
 
-        JPanel panel = new JPanel();
-//επίσης αποτυχημένο button για send αφού υπήρχε θέμα γενικά στο textfield  ( έχει υποθεί στο doc )     
+        JPanel panel = new JPanel();     
         JButton btn = new JButton("send ");
-        
-//για να μη επεκτείνεται το textarea ανάλογα με τον όγκο των δεδομένων    
+          
         panel.setBorder(BorderFactory.createEmptyBorder(50, 10, 10, 10));
 		frame.add(panel);
 		panel.add(tarea, BorderLayout.CENTER);	
@@ -76,27 +73,21 @@ public class Client {
 		panel.add(btn);
 		panel.add(sp);
 	
-
-//ξεκινάμε με sockets		
+		
 		Socket s=new Socket("localhost",port);  
 		DataInputStream din=new DataInputStream(s.getInputStream());  
 		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
 		
-//strS αυτό που στέλνει ο server
 		String strS=""; 
-//τι στέλνω εγώ κλασικά διαβάζοντας γραμμή γιατί δεν δούλεψε με το gui
 		String toSend="";
 		Scanner sc = new Scanner(System.in);
 		
-
-//Υπήρχε θέμα με το while οπότε το έκανα 1-1 ερώτηση απάντηση server client		
-//στέλνει ο server το κατεβατό με τα αρχεία που έχει και ρωτάει επίσης να δώσεις bitrate+format
+		
 			strS=din.readUTF();  
 			System.out.println("Server says: "+strS);  
 			tarea.setText("Server says: "+strS);
 			
-//του στέλνω bitrate και format	σε μορφή XMbps,_type
 			toSend = sc.nextLine();		
 			try {				
 				dout.writeUTF(toSend);
@@ -106,13 +97,11 @@ public class Client {
 			}
 			
 					
-//μου στέλνει ονόματα αρχείων που ταιριάζουν σε αυτά που θέλω
 			strS=din.readUTF();  
 			System.out.println("Server says: "+strS);  
 			tarea.setText("Server says: "+strS);
 			
 			
-//του στέλνω πιο αρχείο θέλω να δω
 			toSend = sc.nextLine();		
 			try {
 				dout.writeUTF(toSend);
@@ -122,12 +111,10 @@ public class Client {
 			}
 			
 				
-//με ρωτάει για πρωτόκολλο
 			strS=din.readUTF();  
 			System.out.println("Server says: "+strS);  
 			tarea.setText("Server says: "+strS);
 					
-//του απαντάω πρωτόκολλο
 			toSend = sc.nextLine();		
 			try {
 				dout.writeUTF(toSend);
@@ -136,8 +123,7 @@ public class Client {
 				e.printStackTrace();
 			}
 						
-//όλα οκ μέχρι εδώ, δεν έχω άλλα πάρε δώσε και πλέον μπορώ να δω το steam	
-//στέλνει ο server την λέξη streaming( αν κάνει stream είμαστε οκ γιατί το ελέγχουμε, αν δεν κάνει stream απλά δεν θα εκτελεστεί ο ffmplay )
+
 			strS=din.readUTF();  
 			System.out.println("Server says: "+strS);  
 			tarea.setText("Server says: "+strS);
